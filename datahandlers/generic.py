3a9332a5e4987bb1562da68d8718000650caf0d6
Max Wagner <max@trollbu.de>
2013-02-26 23:30:34 +0100

    Implement halfway intelligent generic text generator

--- datahandlers/generic.py
+++ datahandlers/generic.py
@@ -2,10 +2,19 @@
 
 
 def parse(lines):
-    lines.append('')
     return '\n'.join(lines)
 
 
 def generate(json):
-    # TODO Think of something sensible to do here
-    return json
+    # We really only want to have to deal with strings here
+    strings = []
+    # we do a best effort on dicts and list, that should be generic enough
+    if type(json) is dict:
+        text = [generate(val) for val in json.values()]
+        text.append('')
+        return '\n'.join(text)
+    if type(json) is list:
+        text = [generate(val) for val in json]
+        text.append('')
+        return '\n'.join(text)
+    return str(json)
